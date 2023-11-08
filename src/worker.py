2ce50282111b98dbfe7572be8f06d545b897a828
SangDoo Nam <kexe009@hotmail.com>
2023-11-09 00:53:09 +0100

    finish

--- src/worker.py
+++ src/worker.py
@@ -3,15 +3,20 @@ from tool import Laptop
 
 
 # Complete the class Worker
-class Worker:
+class Worker(ABC):
+    
     def __init__(self, name):
         self.name = name
-
+    
+    @abstractmethod
     def work(self):
-        print(f"\n\n{self.name} starts working:")
-
+        pass
+    
+    @abstractmethod
     def take_break(self, minutes):
-        print(f"\n\n{self.name} takes {minutes} minutes break:")
+        pass
+    
+
 
 
 # Complete this class, so that it would work properly. Implement the missing methods
@@ -24,20 +29,29 @@ class Programmer(Worker):
         return f"{self.name} codes with {self.language}"
 
     def work(self):
-        super().work()
+        print(f"\n\n{self.name} starts coding in {self.language}.")
+    
+    def take_break(self, minutes):
+        print(f"\n\n{self.name} takes {minutes} minutes break to rest.")
 
 
 # Complete the class Janitor.
 # Janitor is a subclass of the class Worker
 # work(): Janitor fixes pipes with "tool"
 # take_break(): Janitor listens to music
-class Janitor:
+class Janitor(Worker):
     def __init__(self, name, tool):
         super().__init__(name)
         self.tool = tool
 
     def __str__(self):
         return f"{self.name} uses {self.tool}"
+    
+    def work(self):
+        print(f"\n\n{self.name} starts fixing pipes with {self.tool}.")
+    
+    def take_break(self, minutes):
+        print(f"\n\n{self.name} takes {minutes} minutes break to listen to music.")
 
 
 
